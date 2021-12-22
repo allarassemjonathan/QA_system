6e8f4ad5211358ee5bcc63da3a5079e5c2627681
Eli Lowry <lowryec17@gcc.edu>
2021-12-21 19:54:17 -0500

    Kind of working lol

--- QandA.py
+++ QandA.py
@@ -6,6 +6,24 @@ model = s.load("en_core_web_md")
 config = {"punct_chars": None}
 model.add_pipe("sentencizer", config=config)
 
-content = open("Knowledge_Source.txt","r", encoding="utf8").read()
-model.max_length = len(content) + 100
+content = open("Knowledge_Source.txt","r", encoding="utf8").read(1000000)
+# model.max_length = len(content) + 100
 history_doc = model(content, disable = ["ner", "parser"])
+
+q = ""
+while(q != "Quit"):
+    q = input("Ask me a question...(type 'Quit' to quit)")
+    if q == "Quit":
+        break
+    question = model(q)
+    for t in question:
+        if t.is_stop or t.is_space or t.is_punct:
+            pass
+    max_sim = 0
+    answer = None
+    for sentence in history_doc.sents:
+        cur_sim = sentence.similarity(question)
+        if cur_sim > max_sim:
+            max_sim = cur_sim
+            answer = sentence
+    print(answer)
